8af26414ea7e82f3c3f3736a4bd6d9d57a91d5ff
Luis Avila <luis.avila@alunatek.com>
2026-03-24 21:51:47 -0500

    fix: arreglos para la versión del open project desplegada en k8s

--- lib/openproject/capacity_management/engine.rb
+++ lib/openproject/capacity_management/engine.rb
@@ -2,17 +2,20 @@ module OpenProject
   module CapacityManagement
     class Engine < ::Rails::Engine
       engine_name :openproject_capacity_management
+      isolate_namespace CapacityManagement
 
-      initializer 'capacity_management.register_plugin' do
-        OpenProject::Plugins.register :openproject_capacity_management do
-          name 'Capacity Management Plugin'
-          author 'Luis Avila'
-          description 'Plugin to manage team capacity and workload.'
-          version '0.1.0'
-          url 'https://github.com/luisavila/openproject-capacity_management'
-          author_url 'https://github.com/luisavila'
+      initializer 'capacity_management.register' do
+        require 'open_project/plugins'
 
+        OpenProject::Plugin.register :openproject_capacity_management,
+                                      author_url: 'https://github.com/AlunaTek-team/openproject-capacity-plugin' do
           requires_openproject '>= 13.0.0'
+
+          project_module :capacity_management do
+            permission :view_capacity_management, {
+              'capacity_management/dashboard' => [:index, :data]
+            }, public: true
+          end
 
           menu :project_menu,
                :capacity_management_dashboard,
@@ -21,12 +24,6 @@ module OpenProject
                param: :project_id,
                icon: 'icon-context icon-stats',
                after: :overview
-
-          project_module :capacity_management do
-            permission :view_capacity_management, {
-              'capacity_management/dashboard' => [:index, :data]
-            }, public: true
-          end
         end
       end
     end
