5a09349f9ec47619229bc1912b30f6fd6bb6944e
Luis Ávila <luisavila@periferia-it.com>
2026-03-25 15:23:26 -0500

    fix: agregación automatico de campos del plugin en base de datos

--- lib/openproject/capacity_management/engine.rb
+++ lib/openproject/capacity_management/engine.rb
@@ -5,6 +5,18 @@ module OpenProject
 
       class_inflection_override('openproject' => 'OpenProject')
 
+      initializer 'capacity_management.setup_custom_fields' do
+        # Esto se ejecuta automáticamente al arrancar OpenProject
+        if ActiveRecord::Base.connection.table_exists?('custom_fields')
+          begin
+            require_dependency File.expand_path('../../../capacity_management/setup_service', __dir__)
+            CapacityManagement::SetupService.ensure_custom_fields
+          rescue => e
+            Rails.logger.error "Error configurando campos de Capacity Management: #{e.message}"
+          end
+        end
+      end
+
       register 'openproject-capacity_management',
                requires_openproject: '>= 13.0.0',
                author_url: 'https://github.com/AlunaTek-team' do
